dbdb0e224acd4516a31d51eea7ab348355c011e4
wakasa51 <y.takashi51@gmail.com>
2019-09-08 16:24:39 +0900

    Revert "一旦、sellerとcategoriesでN+1を潰してみます。"
    This reverts commit cac658a6b355833d383f294f6b5a9abf249aad95.

--- ruby/lib/isucari/web.rb
+++ ruby/lib/isucari/web.rb
@@ -364,24 +364,7 @@ module Isucari
       items = if item_id > 0 && created_at > 0
         # paging
         begin
-          sql = <<-EOS
-            SELECT items.*,
-                   users.account_name AS u_account_name,
-                   users.num_sell_items AS u_num_sell_items,
-                   categories.parent_id AS c_parent_id,
-                   categories.category_name AS c_category_name,
-                   parent_categories.category_name AS parent_c_category_name
-            FROM `items`
-            LEFT JOIN `users` ON `items`.`seller_id` = `users`.`id`
-            LEFT JOIN `categories` ON `items`.`category_id` = `categories`.`id`
-            LEFT JOIN `categories` AS `parent_categories` ON `categories`.`parent_id` = `parent_categories`.`id`
-            WHERE (`seller_id` = ? OR `buyer_id` = ?)
-              AND `status` IN (?, ?, ?, ?, ?)
-              AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?))
-            ORDER BY `created_at` DESC, `id` DESC
-            LIMIT #{TRANSACTIONS_PER_PAGE + 1}
-          EOS
-          db.xquery(sql, user['id'], user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP, Time.at(created_at), Time.at(created_at), item_id)
+          db.xquery("SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?, ?, ?, ?, ?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}", user['id'], user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP, Time.at(created_at), Time.at(created_at), item_id)
         rescue => e
           logger.info(e.messages)
           db.query('ROLLBACK')
@@ -390,22 +373,7 @@ module Isucari
       else
         # 1st page
         begin
-          sql = <<-EOS
-            SELECT items.*,
-                   users.account_name AS u_account_name,
-                   users.num_sell_items AS u_num_sell_items,
-                   categories.parent_id AS c_parent_id,
-                   categories.category_name AS c_category_name,
-                   parent_categories.category_name AS parent_c_category_name
-            FROM `items`
-            LEFT JOIN `users` ON `items`.`seller_id` = `users`.`id`
-            LEFT JOIN `categories` ON `items`.`category_id` = `categories`.`id`
-            LEFT JOIN `categories` AS `parent_categories` ON `categories`.`parent_id` = `parent_categories`.`id`
-            WHERE (`seller_id` = ? OR `buyer_id` = ?)
-              AND `status` IN (?, ?, ?, ?, ?)
-            ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}
-          EOS
-          db.xquery(sql, user['id'], user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP)
+          db.xquery("SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?, ?, ?, ?, ?) ORDER BY `created_at` DESC, `id` DESC LIMIT #{TRANSACTIONS_PER_PAGE + 1}", user['id'], user['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, ITEM_STATUS_CANCEL, ITEM_STATUS_STOP)
         rescue => e
           logger.info(e.messages)
           db.query('ROLLBACK')
@@ -414,17 +382,17 @@ module Isucari
       end
 
       item_details = items.map do |item|
-        if item['u_account_name'].nil?
+        seller = get_user_simple_by_id(item['seller_id'])
+        if seller.nil?
           db.query('ROLLBACK')
           halt_with_error 404, 'seller not found'
         end
-        seller = get_user_from_item_of_new_items_query(item)
 
-        if item['c_category_name'].nil?
+        category = get_category_by_id(item['category_id'])
+        if category.nil?
           db.query('ROLLBACK')
           halt_with_error 404, 'category not found'
         end
-        category = get_category_from_item_of_new_items_query(item)
 
         item_detail = {
           'id' => item['id'],
